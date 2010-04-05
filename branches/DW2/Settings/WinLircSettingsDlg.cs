d1058944b6161bd77ce3c5b1a5f6e56f33654412
teo.eterovic <teo.eterovic@b4a3c6e6-e7fb-11dd-bd7b-b12b6590754f>
2010-04-05 18:23:28 +0000

    Initial import

---
+++ branches/DW2/Settings/WinLircSettingsDlg.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace VLCTestApp.Settings
+{
+    public partial class WinLircSettingsDlg : Form
+    {
+        private Settings appSettings;
+
+        public WinLircSettingsDlg()
+        {
+            InitializeComponent();
+
+            if (File.Exists("Settings.dat"))
+            {
+                FileStream fs;
+                appSettings = (Settings)(new BinaryFormatter()).Deserialize(fs = File.Open("Settings.dat", FileMode.Open));
+                fs.Close();
+            }
+            else
+            {
+                MessageBox.Show("Settngs file doesn't exists. Creating a new one");
+                appSettings = new Settings();
+            }
+
+            rtmpURI.Text = appSettings.RtmpAddress;
+            epgURI.Text = appSettings.EpgAddress;
+            wlCm.Text = appSettings.Chan;
+            wlFs.Text = appSettings.Full;
+            wlNc.Text = appSettings.Next;
+            wlPc.Text = appSettings.Prev;
+            wlPort.Text = appSettings.Port;
+            wlURI.Text = appSettings.Addr; 
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            appSettings.RtmpAddress = rtmpURI.Text;
+            appSettings.EpgAddress = epgURI.Text;
+            appSettings.Chan = wlCm.Text;
+            appSettings.Full =wlFs.Text;
+            appSettings.Next = wlNc.Text;
+            appSettings.Prev = wlPc.Text;
+            appSettings.Port = wlPort.Text;
+            appSettings.Addr = wlURI.Text;
+
+            FileStream fs;
+            (new BinaryFormatter()).Serialize(fs=File.Open("Settings.dat", FileMode.Create), appSettings);
+            fs.Close();
+            this.Close();
+
+        }
+    }
+}
